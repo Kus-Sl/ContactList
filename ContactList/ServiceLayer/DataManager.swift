44e79a80e3b5b8e5adadb533846fe4500b9e89a7
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-05-07 12:47:47 +0300

    DataManager: - creating a class with contact information

--- ContactList/ServiceLayer/DataManager.swift
+++ ContactList/ServiceLayer/DataManager.swift
@@ -5,4 +5,68 @@
 //  Created by Вячеслав Кусакин on 07.05.2022.
 //
 
-import Foundation
+class DataManager {
+    let names = [
+        "John",
+        "Aaron",
+        "Tim",
+        "Ted",
+        "Steven",
+        "Yosef",
+        "Leonel",
+        "Marcos",
+        "Zeus",
+        "Bowen",
+        "William",
+        "Kayden",
+        "Usman"
+    ]
+
+    let surnames = [
+        "Smith",
+        "Dow",
+        "Isaacson",
+        "Pennyworth",
+        "Jankins",
+        "Gonzales",
+        "Hughes",
+        "Brown",
+        "Flores",
+        "Hayes",
+        "Foster",
+        "Bell",
+        "Baker"
+    ]
+
+    let phones = [
+        "+7(999)057-43-39",
+        "+7(999)419-99-89",
+        "+7(999)706-09-34",
+        "+7(999)778-21-51",
+        "+7(999)186-41-63",
+        "+7(999)735-71-51",
+        "+7(999)579-51-30",
+        "+7(999)091-24-24",
+        "+7(999)019-27-39",
+        "+7(999)642-13-36",
+        "+7(999)052-07-61",
+        "+7(999)142-83-23",
+        "+7(999)532-70-05"
+    ]
+
+    let emails = [
+        "leuzauteproicreu-9104@gmail.com",
+        "pelauquekol@icloud.com",
+        "prezebricrei@mail.ru",
+        "bouqueucraquo@inbox.ru",
+        "naummallawiya8@yandex.ru",
+        "kaukoipeiza@ya.ru",
+        "criddoufrinnacre-6769@gmail.com",
+        "reddattexoi3@list.ru",
+        "quaulogreuk565@icloud.com",
+        "baquasiwiquo@mail.ru",
+        "johazifrefr@mail.ru",
+        "wupresseutaunn@inbox.ru",
+        "grousseupeugaubi1343@gmail.com"
+    ]
+}
