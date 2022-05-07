ed324340b89e652ce8648a1ce575c1e54250a9fd
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-05-07 17:14:47 +0300

    Person model: -  adding logic for creating an array of contacts

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -5,21 +5,35 @@
 //  Created by Вячеслав Кусакин on 07.05.2022.
 //
 
-struct Person {
+struct Person: Equatable {
     let name: String
     let surname: String
-    var fullname: String {
-        name + surname
-    }
     let phone: String
     let email: String
 
-    static func getPerson(data: DataManager) -> Person {
-        Person(
-            name: data.names.randomElement() ?? "",
-            surname: data.surnames.randomElement() ?? "",
-            phone: data.phones.randomElement() ?? "",
-            email: data.emails.randomElement() ?? ""
-        )
+    var fullname: String {
+        name + surname
+    }
+
+    static func getPerson(data: DataManager) -> [Person] {
+        var persons: [Person] = []
+
+        while persons.count < data.names.count {
+            let person = Person(
+                name: data.names.randomElement() ?? "",
+                surname: data.surnames.randomElement() ?? "",
+                phone: data.phones.randomElement() ?? "",
+                email: data.emails.randomElement() ?? ""
+            )
+            !persons.contains(person) ? persons.append(person) : nil
+        }
+        return persons
+    }
+
+    static private func ==(lhs: Person, rhs: Person) -> Bool {
+        lhs.name == rhs.name
+        || lhs.surname == rhs.surname
+        || lhs.phone == rhs.phone
+        || lhs.email == rhs.email
     }
 }
