8fbed375e27c8f0d1995188b9749fe766f048b24
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-05-07 14:09:24 +0300

    Person model: - creating Person struct

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -5,4 +5,21 @@
 //  Created by Вячеслав Кусакин on 07.05.2022.
 //
 
-import Foundation
+struct Person {
+    let name: String
+    let surname: String
+    var fullname: String {
+        name + surname
+    }
+    let phone: String
+    let email: String
+
+    static func getPerson(data: DataManager) -> Person {
+        Person(
+            name: data.names.randomElement() ?? "",
+            surname: data.surnames.randomElement() ?? "",
+            phone: data.phones.randomElement() ?? "",
+            email: data.emails.randomElement() ?? ""
+        )
+    }
+}
